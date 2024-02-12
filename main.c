c3067b1b35d8f9ee10937ab73083c88ea34c4bcc
vraj2711 <156588583+vraj2711@users.noreply.github.com>
2024-02-12 12:19:35 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    int i, j, rows;
-    printf("Enter number of rows : ");
-    scanf("%d", &rows);
-    for (i = 1; i <= rows; i++) {
-        for (j = 1; j <= i; j++)
-            printf("%d", j);
-        printf("\n");
-    }
-	return 0;
+
+int main() {
+    int m[10][10], t[10][10], r, c, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            scanf("%d", &m[i][j]);
+
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            t[j][i] = m[i][j];
+
+    printf("\nOriginal matrix:\n");
+    for (i = 0; i < r; i++, printf("\n"))
+        for (j = 0; j < c; j++)
+            printf("%d\t", m[i][j]);
+
+    printf("\nTranspose of the matrix:\n");
+    for (i = 0; i < c; i++, printf("\n"))
+        for (j = 0; j < r; j++)
+            printf("%d\t", t[i][j]);
+
+    return 0;
 }
 
