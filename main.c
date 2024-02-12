ddf40f5f0c07fb5758060fa54ba15e838a80e979
vraj2711 <156588583+vraj2711@users.noreply.github.com>
2024-02-12 12:10:01 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-int main(){
+int  main() {
     int i, j, rows;
     printf("Enter number of rows : ");
     scanf("%d", &rows);
     for (i = 1; i <= rows; i++) {
         for (j = 1; j <= i; j++)
-            printf("%d", j);
+            printf("*");
+
         printf("\n");
     }
-	return 0;
+    return 0;
 }
-
